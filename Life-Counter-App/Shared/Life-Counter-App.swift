88152c4d10ebbca5f328988c78370f253ec53db3
Ryan-Vakhshoori <96512123+Ryan-Vakhshoori@users.noreply.github.com>
2021-12-21 21:42:10 -0800

    Add files via upload

---
+++ Life-Counter-App/Shared/Life-Counter-App.swift
@@ -0,0 +1,17 @@
+//
+//  mtglifetotalApp.swift
+//  Shared
+//
+//  Created by Ryan Vakhshoori on 7/4/21.
+//
+
+import SwiftUI
+
+@main
+struct mtglifetotalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
